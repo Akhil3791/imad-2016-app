744d63247b2c824ddf0c27b9cddadfd776968523
Akhil3791 <akhilrk91@gmail.com>
2016-10-23 00:40:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,78 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    title:`Article One | Akhil`,
+    heading:'article one',
+    date:`otober 22 2016`,
+    content:`<p> 
+                this is a demo content for article one.. Loreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreum
+            </p>
+             <p> 
+                this is a demo content for article one.. Loreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreum
+            </p>
+             <p> 
+                this is a demo content for article one.. Loreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreumoreum ipsum ipsum loreum loreum ipsum
+                ipsum loreum
+            </p>`
+                
+};
+
+function crateTempalte(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = date.content;
+    var htmlTemplaec=`<html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
+
+
+
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
