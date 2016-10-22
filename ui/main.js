85e8286c6a3d2751431dde21af0652618035185b
Akhil3791 <akhilrk91@gmail.com>
2016-10-23 01:27:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML="this is from javascript";
+
+var image=document.getElementById('madi');
+image.onClick=function(){
+    image.style.marginleft='100px';
+    
+}
