69d7244c455d449fcd042125f7b57fdfe01d83e1
Akhil3791 <akhilrk91@gmail.com>
2016-10-23 03:10:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,7 +66,7 @@ submit.onclick=function(){
   
   var nameInput=document.getElementById('name');
     var nam=nameInput.value;
- request.open('GET','http://akhil3791.imad.hasura-app.io/submit-name?name='+name,true);
+ request.open('GET','http://akhil3791.imad.hasura-app.io/submit-name?name='+ nam,true);
  request.send(null);
     
     
